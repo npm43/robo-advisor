809cd503859624d46700328c8b97302947b61588
npm43 <npm43@georgetown.edu>
2021-03-08 19:20:57 -0500

    LatestClose

--- robo_advisor.py
+++ robo_advisor.py
@@ -3,6 +3,12 @@
 import requests
 import json
 
+
+#utility function to convert float or integer
+#help from Professor Rossetti screencast
+def to_usd(my_price):
+    return"${0:,.2f}".format(my_price)
+
 #
 #INFO OUTPUTS
 #
@@ -10,13 +16,12 @@ import json
 request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=demo"
 
 response = requests.get(request_url)
-#print(type(response))   #> <class 'requests.model.Response'>
-#print(response.status_code) #> 200
-#print(response.text)
 
 parsed_response = json.loads(response.text)
 
 last_refreshed = parsed_response["Meta Data"]["3. Last Refreshed"]
+
+latest_close = parsed_response["Time Series (Daily)"]["2021-03-08"]["4. close"] #> 227.3900
 
 #breakpoint()
 
@@ -30,8 +35,8 @@ print("-------------------------")
 print("REQUESTING STOCK MARKET DATA...")
 print("REQUEST AT: 2018-02-20 02:00pm")
 print("-------------------------")
-print("LATEST DAY:", last_refreshed)
-print("LATEST CLOSE: $100,000.00")
+print(f"LATEST DAY: {last_refreshed}")
+print(f"LATEST CLOSE:{to_usd(float(latest_close))}")
 print("RECENT HIGH: $101,000.00")
 print("RECENT LOW: $99,000.00")
 print("-------------------------")
