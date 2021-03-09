87d37754996ff235330d7545a13aad1f6f137498
npm43 <npm43@georgetown.edu>
2021-03-08 20:20:17 -0500

    CSVSampleData

--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -1,8 +1,10 @@
 # this is the "app/robo_advisor.py" file
 
-import requests
+import csv
 import json
+import os
 
+import requests
 
 #utility function to convert float or integer
 #help from Professor Rossetti screencast
@@ -49,6 +51,20 @@ recent_low = min(low_prices)
 #INFO OUTPUTS
 #
 
+# csv-mgmt/write_teams.py
+#csv_file_path = "data/prices.csv" # a relative filepath
+
+csv_file_path = os.path.join(os.path.dirname(__file__), "..", "data", "prices.csv")
+
+with open(csv_file_path, "w") as csv_file: # "w" means "open the file for writing"
+    writer = csv.DictWriter(csv_file, fieldnames=["city", "name"])
+    writer.writeheader() # uses fieldnames set above
+    writer.writerow({"city": "New York", "name": "Yankees"})
+    writer.writerow({"city": "New York", "name": "Mets"})
+    writer.writerow({"city": "Boston", "name": "Red Sox"})
+    writer.writerow({"city": "New Haven", "name": "Ravens"})
+
+
 print("-------------------------")
 print("SELECTED SYMBOL: XYZ")
 print("-------------------------")
@@ -62,6 +78,8 @@ print(f"RECENT LOW: {to_usd(float(recent_low))}")
 print("-------------------------")
 print("RECOMMENDATION: BUY!")
 print("RECOMMENDATION REASON: TODO")
+print("-------------------------")
+print(f"WRITING DATA TO CSV: {csv_file_path}...")
 print("-------------------------")
 print("HAPPY INVESTING!")
 print("-------------------------")
