d435737a0851b49fcc22b37e3cc805f0a25b5681
npm43 <npm43@georgetown.edu>
2021-03-08 22:58:05 -0500

    ReadMe

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # robo-advisor
+
+# Robo Advisor Project
+
+[Project Description]
+(https://github.com/prof-rossetti/intro-to-python/blob/master/projects/robo-advisor/README.md)
+
+# Installation
+
+Clone or download from [Github source] (https://github.com/npm43/robo-advisor.git), then navigate into the project repository
+
+'''sh
+cd /Users/paulmatsko/Desktop/shopping-cart
+'''
+
+# Usage
+
+Run the program
+
+'''py
+python app/robo_advisor.py
+'''
--- app/robo_advisor.py
+++ app/robo_advisor.py
@@ -3,8 +3,8 @@
 import csv
 import json
 import os
-from datetime import datetime
 from dotenv import load_dotenv
+from datetime import datetime
 import requests
 
 load_dotenv() #> loads contents of the .env file into the script's environment
@@ -19,7 +19,28 @@ def to_usd(my_price):
 #
 
 api_key = os.environ.get("ALPHAVANTAGE_API_KEY")  #"demo"
-symbol = "MSFT" #TODO: ask user for symbol
+symbol = input("Please input the stock symbol you would like to analyze:")  #"MSFT"
+
+#Work with Anthony Redfern to create validation system
+if len(symbol)>1:
+    pass
+if len(symbol)<=5:
+    pass
+else:
+    print("You have not entered a valid symbol. Please try again with a symbol such as AAPL.")
+    exit()
+
+symbol_digits = False
+
+for symbols in symbol:
+    if symbols.isdigit():
+        symbol_digits = True
+
+if symbol_digits == False:
+    pass
+else:
+    print("You have not entered a valid symbol, as you have used a digit. Please try again with a symbol such as AAPL.")
+    exit()
 
 request_url = f"https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol={symbol}&apikey={api_key}"
 
@@ -60,7 +81,15 @@ recent_low = min(low_prices)
 #
 #INFO OUTPUTS
 #
+a = float(latest_close)
+b = float(recent_low)
 
+if a <= (1.15 * b):
+    recommendation = "BUY!"
+    recommendation_reason = "You should buy now because the stock's price has plenty of potential to rise."
+else:
+    recommendation = "DO NOT BUY!"
+    recommendation_reason = "You should not buy now because the stock's price has limited potential to rise."
 
 #csv_file_path = "data/prices.csv" # a relative filepath
 csv_file_path = os.path.join(os.path.dirname(__file__), "..", "data", "prices.csv")
@@ -84,7 +113,7 @@ with open(csv_file_path, "w") as csv_file: # "w" means "open the file for writin
 
 
 print("-------------------------")
-print("SELECTED SYMBOL: XYZ")
+print(f"SELECTED SYMBOL: {symbol}")
 print("-------------------------")
 print("REQUESTING STOCK MARKET DATA...")
 print(f"REQUEST AT: {dt_string}")
@@ -94,8 +123,8 @@ print(f"LATEST CLOSE:{to_usd(float(latest_close))}")
 print(f"RECENT HIGH: {to_usd(float(recent_high))}")
 print(f"RECENT LOW: {to_usd(float(recent_low))}")
 print("-------------------------")
-print("RECOMMENDATION: BUY!")
-print("RECOMMENDATION REASON: TODO")
+print(f"RECOMMENDATION: {recommendation}")
+print(f"RECOMMENDATION REASON: {recommendation_reason}")
 print("-------------------------")
 print(f"WRITING DATA TO CSV: {csv_file_path}...")
 print("-------------------------")
